928a14924425a94c4e536e1622eb61212a739e7e
ondrejtomasek <ondrat2009@gmail.com>
2026-05-07 16:51:27 +0200

    new class for main window

---
+++ src/main/java/org/example/MainWindow.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    private JTextField ingredientField;
+    private JButton generateButton;
+    private JButton favoritesButton;
+
+    public MainWindow() {
+        this.setTitle("Recipe Generator");
+        this.setSize(400, 150);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        JLabel infoLabel = new JLabel("Enter ingredients:");
+        mainPanel.add(infoLabel, BorderLayout.NORTH);
+
+        ingredientField = new JTextField();
+        mainPanel.add(ingredientField, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+
+        generateButton = new JButton("Generate recipe");
+        favoritesButton = new JButton("Favorites");
+
+        buttonPanel.add(generateButton);
+        buttonPanel.add(favoritesButton);
+
+        this.add(mainPanel);
+    }
+
+}
