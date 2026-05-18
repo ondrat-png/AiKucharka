380ca269293e27bd7c140cb31b97cfbca99b2940
ondrejtomasek <ondrat2009@gmail.com>
2026-05-18 13:24:29 +0200

    new method saveRecipesToFile by JSON

--- src/main/java/org/example/FileRecipeStorage.java
+++ src/main/java/org/example/FileRecipeStorage.java
@@ -1,26 +1,63 @@
 package org.example;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class FileRecipeStorage implements RecipeStorage {
 
     private ArrayList<Recipe> saveRecipes;
+    private static final String FILE_NAME = "favorites.json";
+
 
     public  FileRecipeStorage() {
         this.saveRecipes = new ArrayList<>();
+        loadRecipeFromFile();
     }
 
     @Override
     public void saveRecipe(Recipe recipe) throws Exception {
-        if(saveRecipes == null){
+        if(recipe == null){
             throw  new Exception("Recipe can not be null");
         }
         saveRecipes.add(recipe);
         System.out.println("Recipe saved: " + recipe.getTitle());
+        saveRecipesToFile();
     }
 
     @Override
     public ArrayList<Recipe> getAllRecipes() throws Exception {
         return saveRecipes;
+    }
+
+
+    private void saveRecipesToFile() {
+        try {
+            JSONArray jsonArray = new JSONArray();
+
+            for (Recipe recipe : saveRecipes) {
+                JSONObject jsonRecipe = new JSONObject();
+                jsonRecipe.put("title", recipe.getTitle());
+                jsonRecipe.put("ingredients", new JSONArray(recipe.getIngredients()));
+                jsonRecipe.put("instructions", recipe.getInstructions());
+
+                jsonArray.put(jsonRecipe);
+            }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+        writer.write(jsonArray.toString(4));
+        writer.close();
+
+        } catch (Exception e) {
+            System.out.println("Error saving JSON: " + e.getMessage());
+        }
+    }
+
+    public void  loadRecipeFromFile(){
+
+
     }
 }
